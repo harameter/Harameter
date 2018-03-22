17561ffb6fbc51a55e05709ec30296dff7e88657
Owner <harameter506@gmail.com>
2018-03-22 17:23:22 -0500

    merged now removed 'gamemenu' with DashboardActivity

--- app/src/main/java/com/example/harameter/harameter/DashboardActivity.java
+++ app/src/main/java/com/example/harameter/harameter/DashboardActivity.java
@@ -1,7 +1,11 @@
 package com.example.harameter.harameter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
