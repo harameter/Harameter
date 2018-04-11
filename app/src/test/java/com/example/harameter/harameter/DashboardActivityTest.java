725885043140b7f8d723a787c4f21c01c063e4af
Fae <faeidolon@gmail.com>
2018-04-11 12:50:53 -0500

    Added testing files
    Added empty testing files in the correct location

---
+++ app/src/test/java/com/example/harameter/harameter/DashboardActivityTest.java
@@ -0,0 +1,52 @@
+package com.example.harameter.harameter;
+
+import android.content.Context;
+import android.content.Intent;
+
+
+import org.junit.Test;
+
+import java.net.Authenticator;
+
+
+import static org.junit.Assert.*;
+
+public class DashboardActivityTest {
+    protected Context context;
+   // private ActivityLauncherController activityLauncherController;
+  // @Mock Authenticator mockAuthenticator;
+    //@Mock SharedPreferencesManager mockSharedPreferencesManager;
+
+    /*public void setUp()
+    {
+
+    }*/
+
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onClickHaraButton() throws Exception {
+        /*Context spyContext = app(context);
+        Intent expectedIntent = new Intent(spyContext, NextActivity.class);
+
+        activityLauncherController.startNextActivity(spyContext);
+
+        verify(spyContext).startActivity(expectedIntent);*/
+        /*DashboardActivity activity;
+        activity.findViewById(R.id.login).performClick();
+
+        Intent expectedIntent = new Intent(activity, BluetoothActivity.class);
+        Intent actualIntent = getInstance.getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actualIntent.getComponent());*/
+
+    }
+
+    @Test
+    public void onClickAbdominalButton() throws Exception {
+
+    }
+
+}
